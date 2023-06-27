188ffc975b93896c9a14a0480c938b63bd086f53
tezeni <teztig@gmail.com>
2023-06-27 10:16:37 -0700

    alias.c

--- alias.c
+++ alias.c
@@ -5,14 +5,33 @@ void print_alias(alias_t *alias);
 void set_alias(char *var_name, char *value);
 
 /**
+ * print_alias - Prints the alias in the format name='value'.
+ * @alias: Pointer to an alias.
+**/
+void print_alias(alias_t *alias)
+{
+        char *alias_string;
+        int len = _strlen(alias->name) + _strlen(alias->value) + 4;
+
+        alias_string = malloc(sizeof(char) * (len + 1));
+        if (!alias_string)
+                return;
+        _strcpy(alias_string, alias->name);
+        _strcat(alias_string, "='");
+        _strcat(alias_string, alias->value);
+        _strcat(alias_string, "'\n");
+
+        write(STDOUT_FILENO, alias_string, len);
+        free(alias_string);
+}
+
+/**
  * builtin_alias - Builtin command that either prints all aliases, specific
  * aliases, or sets an alias.
  * @args: An array of arguments.
  * @front: A double pointer to the beginning of args.
- *
- * Return: If an error occurs - -1.
- *         Otherwise - 0.
- */
+ * Return: If an error occurs - -1 Otherwise - 0.
+**/
 int builtin_alias(char **args, __silent char **front)
 {
 	alias_t *temp = aliases;
@@ -57,7 +76,7 @@ int builtin_alias(char **args, __silent char **front)
  * 'value' or creates a new alias with 'name' and 'value'.
  * @var_name: Name of the alias.
  * @value: Value of the alias. First character is a '='.
- */
+**/
 void set_alias(char *var_name, char *value)
 {
 	alias_t *temp = aliases;
@@ -91,32 +110,11 @@ void set_alias(char *var_name, char *value)
 }
 
 /**
- * print_alias - Prints the alias in the format name='value'.
- * @alias: Pointer to an alias.
- */
-void print_alias(alias_t *alias)
-{
-	char *alias_string;
-	int len = _strlen(alias->name) + _strlen(alias->value) + 4;
-
-	alias_string = malloc(sizeof(char) * (len + 1));
-	if (!alias_string)
-		return;
-	_strcpy(alias_string, alias->name);
-	_strcat(alias_string, "='");
-	_strcat(alias_string, alias->value);
-	_strcat(alias_string, "'\n");
-
-	write(STDOUT_FILENO, alias_string, len);
-	free(alias_string);
-}
-/**
  * replace_aliases - Goes through the arguments and replace any matching alias
  * with their value.
  * @args: 2D pointer to the arguments.
- *
  * Return: 2D pointer to the arguments.
- */
+**/
 char **replace_aliases(char **args)
 {
 	alias_t *temp;
@@ -150,4 +148,3 @@ char **replace_aliases(char **args)
 
 	return (args);
 }
-
